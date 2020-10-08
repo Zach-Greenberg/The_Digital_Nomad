72d06a8415f009afe01d6093e1b114b1941118dd
Zinc <greenberg.zachary@utexas.edu>
2020-10-08 10:27:58 -0500

    add job and country info

--- countries.js
+++ countries.js
@@ -1,20 +1,11 @@
-function fetchCountries() {
-    let countries = [];
-    var travelURL = "https://travelbriefing.org/" + "Netherlands" + "?format=json";
-    $.ajax({
-        url: travelURL,
-        method: "GET"
-    }).then(function(reply) {
-        console.log(reply);
-
-    })
-}
-
-
-
-
-
-        // let options = '';
+// function fetchCountries() {
+//     let countries = [];
+//     var queryURL = "https://travelbriefing.org/countries.json";
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).done(function(response) {
+//         let options = '';
 //         $.each(response, function (i, countryInfo) {
 //             console.log(i + " " + countryInfo.name);
 //             // countries.push(countryInfo.name);
@@ -32,3 +23,17 @@ function fetchCountries() {
 //         let option = '';
 //     });
 // }
+
+//get working again
+
+function fetchCountries() {
+    let countries = [];
+    var travelURL = "https://travelbriefing.org/Netherlands?format=json";
+    $.ajax({
+        url: travelURL,
+        method: "GET"
+    }).then(function(travelInfo) {
+        console.log(travelInfo);
+
+    })
+}
--- jobs.js
+++ jobs.js
@@ -1,15 +1,34 @@
 var THEMUSE_key="f2c4cefd923645dde166a1740a861046cb505a483466c2d34450aca5116d7571"
 
 function fetchJobs() {
+
+    //why does this work but not on another page
+    // let countries = [];
+    // var travelURL = "https://travelbriefing.org/Japan?format=json";
+    // $.ajax({
+    //     url: travelURL,
+    //     method: "GET"
+    // }).then(function(travelInfo) {
+    //     console.log(travelInfo);
+    //     //create div for 
+    //     var countryDiv = $(`<div class = 'col s12 countryDetails'>`);
+        
+    //     var
+
+
+    // })
+
+
     let  jobs = [];
-    var  queryURL = "https://www.themuse.com/api/public/jobs?category=Finance&page=0";
+    var  queryURL = "https://www.themuse.com/api/public/jobs?category=Finance&page=0";//add ids to 
     $.ajax({
         url: queryURL, 
         method: "GET"
-    }).then(function(response) {
+    }).then(function(response) { //forEach to create what i need into an object and push that into an array
         var jobListing = response.results
         console.log(jobListing);
         for (i = 0; i < 20; i++){ //need to change 20 to jobListing.length
+            console.log(jobListing[i]);
 
             //create div for job information
             var jobDiv = $(`<div class = 'col s12 jobDetails' id = 'jobListing${i}'>`);
@@ -19,13 +38,13 @@ function fetchJobs() {
             jobCompany.text(jobListing[i].company.name);
 
             var jobLocation = $("<p class = jobLocation>");
-            jobLocation.text(jobListing[i].locations.name);
+            jobLocation.text(jobListing[i].locations[0].name);
 
             var jobName = $("<p class = jobName>");
             jobName.text(jobListing[i].name);
 
-            var jobRef = $("<a class = jobRef href = ''>");
-            jobRef.attr("href", (jobListing[i].refs.landing_page));
+            var jobRef = $("<button> Link to Job</button>");
+            jobRef.attr("href", (jobListing[i].refs.landing_page)); //get working
 
             //append all that will be created
             jobDiv.append(jobCompany);
