b5b3b30f21b61d7e3d8a7d5c985d3d644b1bfa79
Zinc <greenberg.zachary@utexas.edu>
2020-10-08 13:55:34 -0500

    test functions

--- jobs.js
+++ jobs.js
@@ -14,7 +14,7 @@ function fetchJobs() {
         }).then(function(response) { //forEach to create what i need into an object and push that into an array
             var jobListing = response.results
             console.log(jobListing);
-            for (i = 0; i < 20; i++){ //need to change 20 to jobListing.length
+            for (i = 0; i < 10; i++){ //need to change 20 to jobListing.length
                 console.log(jobListing[i]);
 
                 //create div for job information
@@ -44,49 +44,49 @@ function fetchJobs() {
     })
 }
 
-const apiKey = "&appid=39889f788ff3fd3d6d6270348600fc5b";
+// const apiKey = "&appid=39889f788ff3fd3d6d6270348600fc5b";
 
   
-function fetchWeather() {
+// function fetchWeather() {
 
-    //add click function to search for job type
-    $("#citySubmit").click(function(event){
-        event.preventDefault();
-        $("#weatherInfo").empty();
+//     //add click function to search for job type
+//     $("#citySubmit").click(function(event){
+//         event.preventDefault();
+//         $("#weatherInfo").empty();
 
-        let  cityName = "Austin";
-        // $("#citySearch").val();
-        console.log(cityName);
-        var  weatherURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + apiKey;
-        $.ajax({
-            url: weatherURL, 
-            method: "GET"
-        }).then(function(weatherResponse) {
-            // var jobListing = response.results
-            console.log(weatherResponse);
+//         let  cityName = "Austin";
+//         // $("#citySearch").val();
+//         console.log(cityName);
+//         var  weatherURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + apiKey;
+//         $.ajax({
+//             url: weatherURL, 
+//             method: "GET"
+//         }).then(function(weatherResponse) {
+//             // var jobListing = response.results
+//             console.log(weatherResponse);
 
-                // //create div for weather information
-                // var weatherDiv = $(`<div class = 'col s12 weahterDetails' id = 'weatherDiv'>`);
+//                 // //create div for weather information
+//                 // var weatherDiv = $(`<div class = 'col s12 weahterDetails' id = 'weatherDiv'>`);
                 
-                // //create h1 and p for information
-                // var jobCompany = $("<h4 class = jobTitle>");
-                // jobCompany.text(jobListing[i].company.name);
+//                 // //create h1 and p for information
+//                 // var jobCompany = $("<h4 class = jobTitle>");
+//                 // jobCompany.text(jobListing[i].company.name);
 
-                // var jobLocation = $("<p class = jobLocation>");
-                // jobLocation.text(jobListing[i].locations[0].name);
+//                 // var jobLocation = $("<p class = jobLocation>");
+//                 // jobLocation.text(jobListing[i].locations[0].name);
 
-                // var jobName = $("<p class = jobName>");
-                // jobName.text(jobListing[i].name);
+//                 // var jobName = $("<p class = jobName>");
+//                 // jobName.text(jobListing[i].name);
 
-                // var jobRef = $("<button target='_blank'>Link to Job</button>");
-                // jobRef.attr("href", (jobListing[i].refs.landing_page)); //get working
+//                 // var jobRef = $("<button target='_blank'>Link to Job</button>");
+//                 // jobRef.attr("href", (jobListing[i].refs.landing_page)); //get working
 
-                // //append all that will be created
-                // jobDiv.append(jobCompany);
-                // jobDiv.append(jobLocation);
-                // jobDiv.append(jobName);
-                // jobDiv.append(jobRef);
-                // $("#resultsContainer").append(jobDiv);
-        })
-    })
-}
+//                 // //append all that will be created
+//                 // jobDiv.append(jobCompany);
+//                 // jobDiv.append(jobLocation);
+//                 // jobDiv.append(jobName);
+//                 // jobDiv.append(jobRef);
+//                 // $("#resultsContainer").append(jobDiv);
+//         })
+//     })
+// }
