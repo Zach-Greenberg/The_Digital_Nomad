feb60046cb35364a068d85d01aeb184e8cd24e90
Zinc <greenberg.zachary@utexas.edu>
2020-10-08 13:37:25 -0500

    job search button

--- jobs.js
+++ jobs.js
@@ -1,63 +1,92 @@
-var THEMUSE_key="f2c4cefd923645dde166a1740a861046cb505a483466c2d34450aca5116d7571"
-
 function fetchJobs() {
 
-    //why does this work but not on another page
-    // let countries = [];
-    // var travelURL = "https://travelbriefing.org/Japan?format=json";
-    // $.ajax({
-    //     url: travelURL,
-    //     method: "GET"
-    // }).then(function(travelInfo) {
-    //     console.log(travelInfo);
-    //     //create div for 
-    //     var countryDiv = $(`<div class = 'col s12 countryDetails'>`);
-        
-    //     var
+    //add click function to search for job type
+    $("#jobSubmit").click(function(event){
+        event.preventDefault();
+        $("#resultsContainer").empty();
 
+        let  jobs = $( "#jobOptions option:selected" ).text();
+        console.log(jobs);
+        var  queryURL = "https://www.themuse.com/api/public/jobs?category=" + jobs + "&page=0";//add ids to 
+        $.ajax({
+            url: queryURL, 
+            method: "GET"
+        }).then(function(response) { //forEach to create what i need into an object and push that into an array
+            var jobListing = response.results
+            console.log(jobListing);
+            for (i = 0; i < 20; i++){ //need to change 20 to jobListing.length
+                console.log(jobListing[i]);
 
-    // })
+                //create div for job information
+                var jobDiv = $(`<div class = 'col s12 jobDetails' id = 'jobListing${i}'>`);
+                
+                //create h1 and p for information
+                var jobCompany = $("<h4 class = jobTitle>");
+                jobCompany.text(jobListing[i].company.name);
 
+                var jobLocation = $("<p class = jobLocation>");
+                jobLocation.text(jobListing[i].locations[0].name);
 
-    let  jobs = [];
-    var  queryURL = "https://www.themuse.com/api/public/jobs?category=Finance&page=0";//add ids to 
-    $.ajax({
-        url: queryURL, 
-        method: "GET"
-    }).then(function(response) { //forEach to create what i need into an object and push that into an array
-        var jobListing = response.results
-        console.log(jobListing);
-        for (i = 0; i < 20; i++){ //need to change 20 to jobListing.length
-            console.log(jobListing[i]);
+                var jobName = $("<p class = jobName>");
+                jobName.text(jobListing[i].name);
 
-            //create div for job information
-            var jobDiv = $(`<div class = 'col s12 jobDetails' id = 'jobListing${i}'>`);
-            
-            //create h1 and p for information
-            var jobCompany = $("<h4 class = jobTitle>");
-            jobCompany.text(jobListing[i].company.name);
+                var jobRef = $("<button target='_blank'>Link to Job</button>");
+                jobRef.attr("href", (jobListing[i].refs.landing_page)); //get working
 
-            var jobLocation = $("<p class = jobLocation>");
-            jobLocation.text(jobListing[i].locations[0].name);
-
-            var jobName = $("<p class = jobName>");
-            jobName.text(jobListing[i].name);
-
-            var jobRef = $("<button> Link to Job</button>");
-            jobRef.attr("href", (jobListing[i].refs.landing_page)); //get working
-
-            //append all that will be created
-            jobDiv.append(jobCompany);
-            jobDiv.append(jobLocation);
-            jobDiv.append(jobName);
-            jobDiv.append(jobRef);
-            $("#resultsContainer").append(jobDiv);
-        }
-
-        //$.each(response, function (i ))
+                //append all that will be created
+                jobDiv.append(jobCompany);
+                jobDiv.append(jobLocation);
+                jobDiv.append(jobName);
+                jobDiv.append(jobRef);
+                $("#resultsContainer").append(jobDiv);
+            }
+        })
     })
 }
 
+const apiKey = "&appid=39889f788ff3fd3d6d6270348600fc5b";
 
+  
+function fetchWeather() {
 
-// work on adding jobDeatils
+    //add click function to search for job type
+    $("#citySubmit").click(function(event){
+        event.preventDefault();
+        $("#weatherInfo").empty();
+
+        let  cityName = "Austin";
+        // $("#citySearch").val();
+        console.log(cityName);
+        var  weatherURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + apiKey;
+        $.ajax({
+            url: weatherURL, 
+            method: "GET"
+        }).then(function(weatherResponse) {
+            // var jobListing = response.results
+            console.log(weatherResponse);
+
+                // //create div for weather information
+                // var weatherDiv = $(`<div class = 'col s12 weahterDetails' id = 'weatherDiv'>`);
+                
+                // //create h1 and p for information
+                // var jobCompany = $("<h4 class = jobTitle>");
+                // jobCompany.text(jobListing[i].company.name);
+
+                // var jobLocation = $("<p class = jobLocation>");
+                // jobLocation.text(jobListing[i].locations[0].name);
+
+                // var jobName = $("<p class = jobName>");
+                // jobName.text(jobListing[i].name);
+
+                // var jobRef = $("<button target='_blank'>Link to Job</button>");
+                // jobRef.attr("href", (jobListing[i].refs.landing_page)); //get working
+
+                // //append all that will be created
+                // jobDiv.append(jobCompany);
+                // jobDiv.append(jobLocation);
+                // jobDiv.append(jobName);
+                // jobDiv.append(jobRef);
+                // $("#resultsContainer").append(jobDiv);
+        })
+    })
+}
